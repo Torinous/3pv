2c59e91d768718738346f9938a55218a79395474
Torinous <Torinous@723274c4-bcd1-11dd-afb1-a7db9ebf18e3>
2010-07-07 23:34:03 +0000

    Issue 36. Перевод на SharpDevelope 3.2

--- Editor/Src/PPPV/Editor/Commands/AboutCommand.cs
+++ Editor/Src/PPPV/Editor/Commands/AboutCommand.cs
@@ -6,29 +6,29 @@ using PPPV.Net;
 
 namespace PPPV.Editor.Commands
 {
-  public class AboutCommand : Command
-  {
-    //Данные
-    private Control sender;
+	public class AboutCommand : Command
+	{
+		//Данные
+		private Control sender;
 
-    //Конструктор
-    public AboutCommand(Control sender_)
-    {
-      Name = "О программе";
-      Description = "Вызов формы \"О программе\"";
-      sender = sender_;
-    }
-    //Методы
-    public override void Execute()
-    {
-      Form f = new AboutForm();
-      f.ShowDialog(sender.FindForm());
-      f.Dispose();
-    }
+		//Конструктор
+		public AboutCommand(Control sender_)
+		{
+			Name = "О программе";
+			Description = "Вызов формы \"О программе\"";
+			sender = sender_;
+		}
+		//Методы
+		public override void Execute()
+		{
+			Form f = new AboutForm();
+			f.ShowDialog(sender.FindForm());
+			f.Dispose();
+		}
 
-    public override void UnExecute()
-    {
-      
-    }
-  }
+		public override void UnExecute()
+		{
+		
+		}
+	}
 }
--- Editor/Src/PPPV/Main.cs
+++ Editor/Src/PPPV/Main.cs
@@ -3,11 +3,10 @@ using System.Windows.Forms;
 
 namespace PPPV
 {
-  internal sealed class Program
-  {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-
-  }
+	internal sealed class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+	}
 }
