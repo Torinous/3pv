60073fc55da4d9ccbd4f9124dd6fd126b1aa6ad2
Torinous <Torinous@723274c4-bcd1-11dd-afb1-a7db9ebf18e3>
2010-10-03 15:51:07 +0000

    Issue 42. Переносим тесты в отдельную сборку.

---
+++ Editor.Tests/Src/Pppv/Editor/Commands/CommandTests.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Torinous
+ * Date: 24.09.2010
+ * Time: 2:41
+ */
+
+namespace Pppv.Editor.Commands.Tests
+{
+   using System;
+   using NUnit.Framework;
+
+   using Pppv.Editor.Commands;
+
+   [TestFixture]
+   public class CommonCommandsTests
+   {
+      [Test]
+      [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "В тестах не важно")]
+      public void NullCommandTest()
+      {
+         Command c = new NullCommand();
+         c.Execute();
+      }
+   }
+}
