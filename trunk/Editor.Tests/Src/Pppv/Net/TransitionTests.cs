d5427d05a16177c958d854417958048d4758e112
Torinous <Torinous@723274c4-bcd1-11dd-afb1-a7db9ebf18e3>
2010-10-03 15:51:07 +0000

    Issue 42. Переносим тесты в отдельную сборку.

---
+++ trunk/Editor.Tests/Src/Pppv/Net/TransitionTests.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Torinous
+ * Date: 24.09.2010
+ * Time: 2:48
+ */
+
+namespace Pppv.Net.Tests
+{
+   using System;
+   using System.Drawing;
+   using System.Drawing.Drawing2D;
+   using NUnit.Framework;
+   using NUnit.Framework.Constraints;
+
+   [TestFixture]
+   public class TransitionTests
+   {
+      [Test]
+      [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "В тестах не важно")]
+      [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", MessageId = "Pppv.Net.Transition", Justification = "В тестах не важно")]
+      public void TestOfConstractions()
+      {
+         Transition transition = new Transition();
+         Assert.That(transition.X, Is.EqualTo(0), "Неверны кординаты Перехода");
+         Assert.That(transition.Y, Is.EqualTo(0), "Неверны кординаты Перехода");
+         transition = new Transition(new Point(0, 0));
+         Assert.That(transition.X, Is.EqualTo(0), "Неверны кординаты Перехода");
+         Assert.That(transition.Y, Is.EqualTo(0), "Неверны кординаты Перехода");
+         transition = new Transition(new Point(10, 10));
+         Assert.That(transition.X, Is.EqualTo(10), "Неверны кординаты Перехода");
+         Assert.That(transition.Y, Is.EqualTo(10), "Неверны кординаты Перехода");
+         transition = new Transition(new Point(-10, -10));
+         Assert.That(transition.X, Is.EqualTo(-10), "Неверны кординаты Перехода");
+         Assert.That(transition.Y, Is.EqualTo(-10), "Неверны кординаты Перехода");
+         transition = new Transition(new Point(-10, 10));
+         Assert.That(transition.X, Is.EqualTo(-10), "Неверны кординаты Перехода");
+         Assert.That(transition.Y, Is.EqualTo(10), "Неверны кординаты Перехода");
+         transition = new Transition(new Point(10, -10));
+         Assert.That(transition.X, Is.EqualTo(10), "Неверны кординаты Перехода");
+         Assert.That(transition.Y, Is.EqualTo(-10), "Неверны кординаты Перехода");
+      }
+   }
+}
