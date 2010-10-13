098c680e7e54fdbf45394d9c11a3539b5df52752
Torinous <Torinous@723274c4-bcd1-11dd-afb1-a7db9ebf18e3>
2010-10-13 18:31:54 +0000

    Issue 45. Рефакторинг Prolog подсистемы

--- Prolog/temporalproperties.pl
+++ Prolog/temporalproperties.pl
@@ -13,13 +13,17 @@
 
 
 /*Живость сети*/
-liveness:-init(S),transition(T),
-           not(implies(init,all(pot(enable(T)))),M),
-           !,fail.
+liveness:-
+	init(_),
+	transition(T),
+        not(implies(init,all(pot(enable(T)))),M),!,
+	fail.
 liveness.
 
 /*Живость перехода*/
-live(T):-init(S),implies(init,all(pot(enable(T))),S).
+live(T):-
+	init(S),
+	implies(init, all(pot(enable(T))), S).
 
 /*Мёртвость перехода*/
 dead(T):- init(S),implies(init,all(not(enable(T))),S).
@@ -36,10 +40,12 @@ potlive(T):-init(S),implies(init,pot(all(pot(enable(T)))),S).
 
 
 /*Потенциальная живость сети*/
-potliveness:-init(S),transition(T),
-             not(implies(init,pot(all(pot(enable(t))))),S),
-             !,fail.
-             potliveness.
+potliveness:-
+	init(S),
+	transition(_),
+        not(implies(init,pot(all(pot(enable(t))))),S),
+        !,fail.
+        potliveness.
 
 /*Детерминированность срабатывания перехода*/
 determ(T):-init(S),implies(init,all(implies(enable(T),pretilda(fired(T)))),S).
